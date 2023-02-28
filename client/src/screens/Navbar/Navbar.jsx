0672b4b41791da26ca7645a35783f1515ed383ae
sodizdaroglu <americanlaz@gmail.com>
2023-02-28 16:40:17 -0500

    component

--- client/src/screens/Navbar/Navbar.jsx
+++ client/src/screens/Navbar/Navbar.jsx
@@ -3,7 +3,16 @@ import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+        <div className='navbar-name'>Sukru Oguz Dizdaroglu</div>
+        <div className='navbar-links-container'>
+            <a href='#home'>Home</a>
+            <a href='#aboutme'>About Me</a>
+            <a href='#technologies'>Technologies</a>
+            <a href='#portfolio'>Portfolio</a>
+            <a href='#contact'>Contact</a>
+        </div>
+    </div>
   )
 }
 
