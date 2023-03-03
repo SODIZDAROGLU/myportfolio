a32521c4ebfb78669c663bb765d1f34c1e8a6144
sodizdaroglu <americanlaz@gmail.com>
2023-03-03 13:39:56 -0500

    hamburgerMenu

--- client/src/screens/Navbar/Navbar.jsx
+++ client/src/screens/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import "./Navbar.css"
+import React from 'react';
+import "./Navbar.css";
+import hamburger from './hamburger-menu.svg';
 
 function Navbar() {
   return (
     <div className='navbar-container'>
-        <div className='navbar-name'>Sukru Oguz Dizdaroglu</div>
+        <div className='navbar-name'>
+           <div>Sukru Oguz Dizdaroglu</div> 
+           <img src={hamburger} alt="hamburgerNenu"  />
+            </div>
         <div className='navbar-links-container'>
             <a href='#home'>Home</a>
             <a href='#aboutme'>About Me</a>
